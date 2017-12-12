5396158194995233066a15f2591286eab5541a70
gdomod <root@gdomod.de>
2017-12-12 16:43:43 +0100

    Init

---
+++ UIconfig.js
@@ -0,0 +1,15 @@
+const CONFIG = {
+    headless: true,
+    api: {
+        host: '0.0.0.0',
+        port: 3000,
+    },
+    ui: {
+        ssl: false,
+        host: '0.0.0.0', // Set this to the IP of the machine that will run Gekko
+        port: 3000,
+        path: '/'
+    },
+    adapter: 'sqlite'
+
+}
